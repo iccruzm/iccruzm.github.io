5012ea87b2d0b6755d2dd3664edb9baab17aa69c
programmationlogic <92724090+iccruzm@users.noreply.github.com>
2024-09-20 01:04:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Importamos el componente App desde App.js
+import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App /> // Renderizamos el componente App
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
